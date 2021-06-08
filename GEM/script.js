29901d87d79e1a44d0e7113d42350b33e8cf364e
omkrishna <omkrishna.ok@gmail.com>
2021-06-08 20:15:40 +0530

    section 2 and 3

--- GEM/script.js
+++ GEM/script.js
@@ -10,4 +10,62 @@ function myMap() {
   var map4 = new google.maps.Map(document.getElementById("map4"), mapProp);
   var map5 = new google.maps.Map(document.getElementById("map5"), mapProp);
   var map6 = new google.maps.Map(document.getElementById("map6"), mapProp);
+
+  var s2map1 = new google.maps.Map(
+    document.getElementById("section-2-map1"),
+    mapProp
+  );
+  var s2map2 = new google.maps.Map(
+    document.getElementById("section-2-map2"),
+    mapProp
+  );
+  var s2map3 = new google.maps.Map(
+    document.getElementById("section-2-map3"),
+    mapProp
+  );
+  var s2map4 = new google.maps.Map(
+    document.getElementById("section-2-map4"),
+    mapProp
+  );
+  var s2map5 = new google.maps.Map(
+    document.getElementById("section-2-map5"),
+    mapProp
+  );
+  var s2map6 = new google.maps.Map(
+    document.getElementById("section-2-map6"),
+    mapProp
+  );
+  var s2map7 = new google.maps.Map(
+    document.getElementById("section-2-map7"),
+    mapProp
+  );
+
+  var s3map1 = new google.maps.Map(
+    document.getElementById("section-3-map1"),
+    mapProp
+  );
+  var s3map2 = new google.maps.Map(
+    document.getElementById("section-3-map2"),
+    mapProp
+  );
+  var s3map3 = new google.maps.Map(
+    document.getElementById("section-3-map3"),
+    mapProp
+  );
+  var s3map4 = new google.maps.Map(
+    document.getElementById("section-3-map4"),
+    mapProp
+  );
+  var s3map5 = new google.maps.Map(
+    document.getElementById("section-3-map5"),
+    mapProp
+  );
+  var s3map6 = new google.maps.Map(
+    document.getElementById("section-3-map6"),
+    mapProp
+  );
+  var s3map7 = new google.maps.Map(
+    document.getElementById("section-3-map7"),
+    mapProp
+  );
 }
